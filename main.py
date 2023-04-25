042a6c473665cd7fb856476e90fac14a08c9ad9c
Sakuru <74860621+SakuruWasTaken@users.noreply.github.com>
2023-04-24 19:00:45 -0700

    improve rod display code

--- main.py
+++ main.py
@@ -24,17 +24,6 @@ class simulator:
 
         self.scram_timer = -1
 
-        # stuff for physics
-        # all of this stuff related to pysics is currently unused experimental stuff
-
-        self.reactivity = 0.00
-        self.control_rod_coefficient = 44.2043795620438
-        self.void_coefficient = -7.9543795620438
-        self.heat = 290.00
-        self.heat_coefficient = -36.25
-
-        self.aprm = 100.0
-
         # actually running the simulator
 
         self.layout = rods_helper.generate_control_rods()
@@ -47,8 +36,6 @@ class simulator:
             rods_helper.remove_group(group)
             group += 1
         self.model_timer()
-
-        
 
 
     def withdraw_selected_cr(self, continuous = False):
@@ -201,8 +188,7 @@ class simulator:
                         if not rod_number in self.moving_rods:
                             self.moving_rods.append(rod_number)
                         # the time from full out to full in is around ~2.6 seconds
-                        # random is to give the appearance of some rods being faster/slower than others
-                        cr_insertion -= (2.23)
+                        cr_insertion -= 2.23
                         if cr_insertion <= 0:
                             cr_insertion = 0
                 else:
@@ -213,34 +199,10 @@ class simulator:
 
             glob.control_rods[rod_number].update(cr_insertion=cr_insertion, cr_scram=cr_scram, cr_accum_trouble=cr_accum_trouble, cr_drift_alarm=cr_drift_alarm)
 
-    def physics_cycle(self):
-        return
-        # unused experimental stuff
-
-        # TODO: calculate physics for each rod in reactor
-
-        # calculate control rod coefficient
-        #control_rods = glob.db.execute("SELECT cr_insertion FROM control_rods")
-        #print(glob.db.execute("SELECT COUNT(*) FROM control_rods"))
-        #rods = []
-        #for rod in control_rods:
-        #    rods.append(float(rod[0]))
-        #average_insertion = statistics.mean(rods)
-        # TODO: calculate worth for individual rods
-        #self.control_rod_coefficient = average_insertion
-        #print(average_insertion)
-        #print(self.control_rod_coefficient)
-
-        # calculate heat coefficient
-        #self.heat_coefficient = (self.heat - (self.heat/8)) - self.heat
-        #print(self.heat_coefficient)
-
-
     def model_timer(self):
         # TODO: proper timer instead of just time.sleep
         while True:
             threading.Thread(target=lambda: self.control_rods_cycle(), daemon=False).start()
-            threading.Thread(target=lambda: self.physics_cycle(), daemon=False).start()
             # TODO: only calculate when rods are moved
             # TODO: finish group calculation
             #threading.Thread(target=lambda: rods_helper.calculate_current_group(), daemon=False).start()
@@ -250,34 +212,15 @@ class simulator:
 
 
     def reset_scram(self):
-        if self.scram_timer == 0:
-            for rod_number in glob.control_rods.items():
-                glob.control_rods[rod_number].update(cr_scram=False, cr_accum_trouble=False, cr_drift_alarm=False)
-            glob.rod_withdraw_block = False
-            glob.rod_insert_block = False
-            self.scram_active = False
-            self.scram_timer = -1
-
-    def rod_display(self):
-        # TODO: move this into the GUI code
-        rods_printed_row = 0
-        y = 0
-        final_message = ""
+        self.scram_active = False
+        self.scram_timer = -1
+        # short pause to wait for control_rods_cycle to finish
+        time.sleep(0.1)
+        glob.rod_withdraw_block = False
+        glob.rod_insert_block = False
         for rod_number, rod_info in glob.control_rods.items():
-            # i know there are better ways to do this, but this is temporary code anyways (as i am planning on implementing a realistic full core display), so i don't care.
-            rod_insertion = int(rod_info["cr_insertion"])
-            if y != int(rod_number.split("-")[1]):
-                final_message = f"{final_message}\n"
-            y = int(rod_number.split("-")[1])
+            glob.control_rods[rod_number].update(cr_scram=False, cr_accum_trouble=False, cr_drift_alarm=False)
 
-            if rod_number in self.moving_rods:
-                rod_insertion = "--"
-
-            if len(str(rod_insertion)) == 1:
-                rod_insertion = f"0{rod_insertion}"
-
-            final_message = f"{final_message}|{rod_insertion}{'sel' if self.selected_cr == rod_number else ''}"
-        return final_message # goodbye
 
 
     def run_gui(self, layout):
@@ -303,14 +246,18 @@ class simulator:
         ]
         ]
         # TODO: mode switch
-        core = self.rod_display().split("\n")
         rods_number = 0
-        for line in core:
-            rods = []
-            for rod in line.split("|"):
-                rods.append(sg.Text(rod, justification="center", key=f"ROD_DISPLAY_{str(rods_number)}", text_color='darkred', pad=(2,2)))
-                rods_number += 1
-            column_3.append(rods)
+        rods = []
+        y = 59
+        for rod_number, rod_info in glob.control_rods.items():
+            new_y = rod_number.split("-")[1]
+            if new_y != y:
+                column_3.append(rods)
+                rods = []
+            rods.append(sg.Text("00", justification="center", key=f"ROD_DISPLAY_{rod_number}", text_color='darkred', pad=(2,2)))
+            rods_number += 1
+            y = new_y
+        column_3.append(rods)
 
         # Create the window
         layout = [[sg.Column(column_1, element_justification='c'),
@@ -333,19 +280,19 @@ class simulator:
                 window["insert_lt"].update(text_color="darkred" if self.cr_direction == 1 else "greenyellow")
                 window["settle_lt"].update(text_color="darkred" if self.cr_direction == 3 else "greenyellow")
 
-                core = self.rod_display().split("\n")
-                rods_number = 0
-                for line in core:
-                    rods = line.split("|")
-                    for rod in rods:
-                        font = ("monospace", 15)
-                        # check if rod is selected
-                        if len(rod) >= 5:
-                            font = ("monospace", 18)
-                            rod = rod[:-3] # <--- cat face
-                        color = "darkred" if rod == "48" or rod == "--" else "greenyellow" if rod == "00" else "black" if rod == "49" else "orange" 
-                        window[f"ROD_DISPLAY_{str(rods_number)}"].update(rod, text_color=color, font=font)
-                        rods_number += 1
+                for rod_number, rod_info in glob.control_rods.items():
+                    rod_insertion = int(rod_info["cr_insertion"])
+                    color = "darkred" if rod_insertion == 48 or rod_number in self.moving_rods else "greenyellow" if rod_insertion == 0 else "black" if rod_insertion == 49 else "orange" 
+                    if len(str(rod_insertion)) == 1:
+                        rod_insertion = f"0{rod_insertion}"
+                    if rod_number in self.moving_rods:
+                        rod_insertion = "--"
+                    font = ("monospace", 15)
+                    # check if rod is selected
+                    if rod_number == self.selected_cr:
+                        font = ("monospace", 18)
+                    
+                    window[f"ROD_DISPLAY_{rod_number}"].update(rod_insertion, text_color=color, font=font)
             elif len(event) == 5 and "-" in event:
                 # we can assume it's a rod
                 # TODO: remove selected value from db and just use self.selected_cr
